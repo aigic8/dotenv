45f00bcbee12aa97ffe803a4f883c51c0e1c2839
aigic8 <aigic.dev@protonmail.com>
2024-08-02 01:23:05 +0330

    added ff and aliases

--- zsh/env.sh
+++ zsh/env.sh
@@ -19,3 +19,13 @@ GO_BIN="/usr/local/go/bin"
 if [ -d "$GO_BIN" ]; then
 	export PATH=$PATH:/usr/local/go/bin
 fi
+
+# aliases and functions
+alias open="xdg-open"
+alias tmux="TERM=alacritty-direct tmux"
+
+ff() {
+	DIR_PATH="$(find $HOME/main $HOME/lab -maxdepth 1 -and -type d | fzf)"
+	BASE_NAME="$(basename $DIR_PATH)"
+	tmux new -c $DIR_PATH -s $BASE_NAME
+}
