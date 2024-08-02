5226d31ce02e30a7ac3f76782fb4c15bdb4a1d4d
aigic8 <aigic.dev@protonmail.com>
2024-08-02 16:52:38 +0330

    added markdown.nvim

--- nvim/lua/lazy_plugins/basic.lua
+++ nvim/lua/lazy_plugins/basic.lua
@@ -107,6 +107,15 @@ return {
 	},
 	{ "rebelot/kanagawa.nvim", config = kanagawa_theme_config },
 	{ "norcalli/nvim-colorizer.lua", config = colorizer_config },
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		main = "render-markdown",
+		opts = {},
+		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	},
 	-- { "mofiqul/vscode.nvim", config = vscode_theme_config },
 	-- {
 	-- 	"folke/tokyonight.nvim",
